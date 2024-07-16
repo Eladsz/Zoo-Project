9a3c878086eb5c864fceeb86d6f05ae84ecb7728
Eladsz <elad.sztejnworcel@solaredge.com>
2024-07-16 10:16:25 +0300

    Update BuyTicketMenu.java

--- src/UI/Menus/BuyTicketMenu.java
+++ src/UI/Menus/BuyTicketMenu.java
@@ -7,8 +7,8 @@ public class BuyTicketMenu extends Menu {
 
 	public BuyTicketMenu() {
 		super("Buy Ticket Menu");
-		AddOption("Buy Ticket", v-> Output.execute(VisitorManagementSystem.getInstance().buyTicket(), "Ticket purchasing", VisitorManagementSystem.getInstance().getLastPurchasedItem()));
-		AddOption("Buy Subscription", v-> Output.execute(VisitorManagementSystem.getInstance().buySubscription(), "Subscription purchasing" ,VisitorManagementSystem.getInstance().getLastPurchasedItem()));
+		AddOption("Buy Ticket", 		v-> Output.execute(VisitorManagementSystem.getInstance().buyTicket(), "Ticket purchasing", VisitorManagementSystem.getInstance().getLastPurchasedItem()));
+		AddOption("Buy Subscription", 	v-> Output.execute(VisitorManagementSystem.getInstance().buySubscription(), "Subscription purchasing" ,VisitorManagementSystem.getInstance().getLastPurchasedItem()));
 	}
 	
 }
